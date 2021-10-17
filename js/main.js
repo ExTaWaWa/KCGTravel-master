32467b26a402c13a5be015d3f56ce7d92940a724
ExTaWaWa <nienchan@gmail.com>
2021-10-17 23:02:13 +0800

    2021/10/17 update
    寫了些JS開頭，優化CSS細節

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//選單
+let locations = document.getElementById('location');
+//四個熱門按鈕
+let buttons = document.getElementsByTagName('button');
+//地區標頭
+let placeTitle = document.querySelector('.placeTitle');
+//地區內容清單
+let placeList = document.querySelector('.placeList');
